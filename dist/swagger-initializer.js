892ba201e5db23b27dca8cf6d9be4489edeb6d43
Timothy Miller <TimothyM@quinnox.com>
2022-09-07 12:56:26 -0500

    push

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "Tim-Org-api-documentation-1.0-swagger.yaml",
+    url: "swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
